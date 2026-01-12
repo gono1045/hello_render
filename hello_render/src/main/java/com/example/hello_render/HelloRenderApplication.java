c45c4eb6c0fa61bcf4f813aca568fea6058b70b4
gono1045 <tequilasunrise1226@gmail.com>
2026-01-12 18:34:14 +0900

    Initial Hello World Spring Boot

---
+++ hello_render/src/main/java/com/example/hello_render/HelloRenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello_render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRenderApplication.class, args);
+	}
+
+}
